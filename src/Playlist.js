135fb362d742e1087aaa84316ef29b757b656c91
Mohawk <mohawk1999@hotmail.nl>
2024-09-25 14:19:08 +0200

    Adding components

---
+++ src/Playlist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Playlist(props){
+        
+    return <div>
+
+        <ul>
+            <li>Strangers - BMTH</li>
+            <li>Little lion man - Mumford and Sons</li>
+        </ul>
+
+
+    </div>
+
+}
+
+export default Playlist;
---
+++ src/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchBar(){
+
+    return (
+        <div>
+            <input type="text"></input>
+            <button>search</button>
+        </div>
+    )
+}
+
+export default SearchBar;
