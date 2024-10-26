16dba733105a2b6423b46bd6a0d7d180c2589f84
Mohawk <mohawk1999@hotmail.nl>
2024-10-26 16:44:56 +0200

    function to add playlist to spotify works!

--- src/App.js
+++ src/App.js
@@ -5,34 +5,6 @@ import SearchBar from './SearchBar/SearchBar';
 import SearchResults from './SearchResults/SearchResults';
 import Spotify  from './Spotify.module';
 
-const songs = [
-  {
-          "key": 1,
-          "name": "Wal-Dryl",
-          "artist": "Tamera",
-          "album": "Diphenhydramine HCl",
-          "uri": "spotify:track:5DQIZ8XC0EP7cfRFrtzY5h"
-        }, {
-          "key": 2,
-          "name": "Pyrantel Pamoate",
-          "artist": "Jackquelin",
-          "album": "PinwormTreatment",
-          "uri": "spotify:track:2plbrEY59IikOBgBGLjaoe"
-        }, {
-          "key": 3, 
-          "name": "Chap-et",
-          "artist": "Molli",
-          "album": "White Petrolatum",
-          "uri":"spotify:track:7j31rVgGX9Q2blT92VBEA0"
-        }, {
-          "key": 4,
-          "name": "childrens pain and fever",
-          "artist": "Benjamen",
-          "album": "Acetaminophen",
-          "uri":"spotify:track:3Z6ReZyj8uKeiGUYf0JElU"
-        }
-  ]
-
 function App() {
 
   //belown function search through a specific array and puts this in a list in the results area//
@@ -54,17 +26,16 @@ const[playlist,setPlaylist] = useState([]);
 
     function handlePlaylist(e){
       let adSong = search.filter((person) => person.id == e.target.parentNode.id)
-      if(!playlist.filter(person => person.id == adSong[0].id)){
+      if(!playlist.some((person) => person.id === e.target.parentNode.id)){
         setPlaylist((prev) => [...prev,adSong[0]])
       }
-        console.log(playlist.filter(person => person.id == e.target.parentNode.id))
-        console.log(playlist)
+        console.log(`Playlist:${playlist.map((person) => person.name)}`); 
     }
 
 //Below code gives the user the choice to remove a track from their custom playlist with the "DEL" button//
     function handleDelete(e){
-      const id= e.target.parentNode.children[0].id;
-      setPlaylist(playlist.filter((item) =>item.key !== id))
+      const id= e.target.parentNode.id;
+      setPlaylist(playlist.filter((item) => item.id !== id))
     }
 
 //Managing the naming of the playlist//
@@ -78,10 +49,18 @@ function handlePlayListName(e){
 
 const [spotplaylist,setSpotplaylist] = useState([]);
 
-function handleSaveToSpotify(e){
-    setSpotplaylist((playlist.map((item)=>item.uri)));
-    setPlaylist([]);
-    console.log(spotplaylist);
+//function handleSaveToSpotify(){
+    //setSpotplaylist((playlist.map((item)=>item.uri)));
+    //setPlaylist([]);
+    //console.log(spotplaylist);
+//}
+
+function handleSaveSpotify(){
+    const uris = playlist.map((item)=>item.uri)
+    Spotify.savePlaylist(playListName,uris).then(() => {
+      setPLayListName("");
+      setPlaylist([])
+    });
 }
   return (
     <div className="App">
@@ -91,7 +70,7 @@ function handleSaveToSpotify(e){
       </div>
       <div className='main'>
       <SearchResults array={search} Click={handlePlaylist}/>
-      <Playlist array={playlist} delButton={handleDelete} playListName={playListName} playListNameChange={handlePlayListName} SaveSpotify={handleSaveToSpotify}/>
+      <Playlist array={playlist} delButton={handleDelete} playListName={playListName} playListNameChange={handlePlayListName} SaveSpotify={handleSaveSpotify}/>
 
       </div>
     </div>
--- src/Spotify.module.js
+++ src/Spotify.module.js
@@ -45,6 +45,38 @@ const Spotify = {
         uri: track.uri
       }));
     });
+  },
+
+  savePlaylist(name,uriList){
+    const accesstoken = Spotify.getAccessToken();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    let userId;
+    return fetch('https://api.spotify.com/v1/me',{
+      headers:{
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then(response =>{
+      return response.json();
+    }).then(jsonResponse =>{
+      userId = jsonResponse.id;
+      console.log(userId);
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,{
+        headers:headers,
+          method:'POST',
+          body: JSON.stringify({name:name})
+      }).then(response =>{
+        return response.json()
+      }).then(jsonResponse =>{
+        const playlistId = jsonResponse.id;
+        console.log(jsonResponse);
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,{
+          headers:headers,
+          method:"POST",
+          body:JSON.stringify({uris:uriList})
+        });
+      })
+    })
+
   }
 }
 
